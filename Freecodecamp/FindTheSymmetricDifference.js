020b03388d233bf564f4889342a1de22292b8f1c
Patrick Jiang <patrick.jiang86@gmail.com>
2020-11-08 23:27:24 -0500

    Algorithms: Find the Symmetric Difference

---
+++ Freecodecamp/FindTheSymmetricDifference.js
@@ -0,0 +1,33 @@
+/**
+ * Algorithms: Find the Symmetric Difference
+    The mathematical term symmetric difference (△ or ⊕) of two sets
+    is the set of elements which are in either of the two sets but not
+      in both. For example, for sets A = {1, 2, 3} and B = {2, 3, 4},
+      A △ B = {1, 4}.
+
+Symmetric difference is a binary operation,
+ which means it operates on only two elements.
+  So to evaluate an expression involving symmetric differences among three
+   elements (A △ B △ C), you must complete one operation at a time.
+   Thus, for sets A and B above,
+   and C = {2, 3}, A △ B △ C = (A △ B) △ C = {1, 4} △ {2, 3} = {1, 2, 3, 4}.
+ */
+
+function sym() {
+  const args = [...arguments];
+  let result = [];
+  for (let i = 0; i < args.length; i++) {
+    var unique = args[i].filter((v, i, a) => a.indexOf(v) === i);
+    unique.forEach((element) => {
+      if (!result.includes(element)) {
+        result.push(element);
+      } else {
+        let index = result.findIndex((el) => el === element);
+        result.splice(index, 1);
+      }
+    });
+  }
+  return result;
+}
+
+console.log(sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]));
