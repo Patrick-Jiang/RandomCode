418206a0669d2e73caf0d4a0e8069ec43f6ff861
Patrick Jiang <patrick.jiang86@gmail.com>
2020-11-08 21:13:55 -0500

    Add counting sore

---
+++ JavascriptSortAlgorithm/QuickSort.js
@@ -0,0 +1,33 @@
+#!/usr/bin/node
+
+const testArr = [7, 5, 2, 4, 3, -9];
+const textArrTwo = [20, 23, -23, 123, 45, 78, 88];
+const textArrThree = [-2, -31, 100, 12, 1, 2, 3, 4, 5, 6];
+function quickSort(arr) {
+  if (arr.length === 1) {
+    return arr;
+  }
+  const piovt = arr[arr.length - 1];
+  const leftArr = [];
+  const rightArr = [];
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] > piovt) {
+      rightArr.push(arr[i]);
+    } else {
+      leftArr.push(arr[i]);
+    }
+  }
+  if (leftArr.length > 0 && rightArr.length > 0) {
+    return [...quickSort(leftArr), piovt, ...quickSort(rightArr)];
+  } else if (leftArr.length > 0) {
+    return [...quickSort(leftArr), piovt];
+  } else {
+    return [piovt, ...quickSort(rightArr)];
+  }
+
+  return arr;
+}
+
+console.log(quickSort(testArr));
+console.log(quickSort(textArrTwo));
+console.log(quickSort(textArrThree));
